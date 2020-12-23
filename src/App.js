fe609b95373bced185c7d5997d44d2c2fa3f1c20
brandon9598 <brandon9598@gmail.com>
2020-12-22 19:44:59 -0500

    hooked up app to api.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Row from "./Row";
+import requests from "./request";
 
 function App() {
   return (
     <div className="App">
       <h1>Hey</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
