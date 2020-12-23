5ad8e9ef0b0258ba0c0d094f002595074c78a55e
brandon9598 <brandon9598@gmail.com>
2020-12-22 20:45:15 -0500

    update banner. update row.

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   // When row loads, pull info from db
@@ -17,14 +18,21 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
-
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
-          <img src={base_url + movie.poster_path} alt={movie.name} />
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={
+              isLargeRow
+                ? base_url + movie.poster_path
+                : base_url + movie.backdrop_path
+            }
+            alt={movie.name}
+          />
         ))}
       </div>
       {/* container -> posters */}
--- src/request.js
+++ src/request.js
@@ -3,7 +3,7 @@ import { MovieConfig } from "./config";
 const requests = {
   fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${MovieConfig.api_key}&language=en-US`,
   fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${MovieConfig.api_key}&with_network=213`,
-  fetchTopRated: `https://api.themoviedb.org/3/discover/top_rated?api_key=${MovieConfig.api_key}&language=en-US`,
+  fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${MovieConfig.api_key}&language=en-US`,
   fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${MovieConfig.api_key}&with_genres=28`,
   fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${MovieConfig.api_key}&with_genres=35`,
   fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${MovieConfig.api_key}&with_genres=27`,
